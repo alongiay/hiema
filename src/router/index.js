748803c23f570a6d441f38e42262fc767d298676
pljayplpl <990969192@qq.com>
2022-06-26 11:10:06 +0800

    项目初始化

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-
+import Layout from '@/views/Layout'
 Vue.use(VueRouter)
 
 const routes = [
-
+  {
+    path: '/', component: Layout
+  }
 ]
 
 const router = new VueRouter({
