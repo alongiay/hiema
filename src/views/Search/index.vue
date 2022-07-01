1d57abd57c2d0bb083b88fac1bdb919ad98c964a
pljayplpl <990969192@qq.com>
2022-07-01 15:31:11 +0800

    111

---
+++ src/views/Search/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <van-search
+      v-model="searchText"
+      show-action
+      placeholder="请输入搜索"
+      background="#3296fa"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  created () { },
+  data () {
+    return {
+      searchText: ''
+    }
+  },
+  methods: {},
+  computed: {},
+  watch: {},
+  filters: {},
+  components: {}
+}
+</script>
+
+<style scoped>
+</style>
